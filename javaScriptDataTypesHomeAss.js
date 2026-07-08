16cb2ce1b86895f7269b50735835854872a56360
mailtoanitha <mailtoanitha2023@gmail.com>
2026-07-08 11:53:19 +0530

    Add files via upload
    JavaScript DataTypes and variables home assignment  - Day 1

---
+++ javaScriptDataTypesHomeAss.js
@@ -0,0 +1,20 @@
+let userName="Mani"
+console.log(userName);
+console.log(typeof userName);
+
+
+let organization="TestLeaf"
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber=9087654321
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+let knowsAutomation= true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
